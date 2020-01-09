14fd19b6dbf3b4a01d04b5bddcc343710ac7dd70
jialeilei <jialeilei@qfang.com>
2020-01-09 09:31:31 +0800

    修复多个点击修改后点击某一个保存导致全部input都再次禁用的问题

--- components/todo-list.js
+++ components/todo-list.js
@@ -11,18 +11,22 @@
       ul.addEventListener('click', this.handleToDo.bind(this))
       shadowRoot.appendChild(ul)
     }
-    attributeChangedCallback() {
-      console.log('attr is update')
+    attributeChangedCallback(attr) {
+      console.log('attr is update', attr, this.getAttribute('table-data'))
       this.handleTableData()
     }
     handleTableData() {
       this.tableData = JSON.parse(this.getAttribute('table-data') || '[]')
-      const liEls = this.tableData.map((todo, index) => {
+      const liEls = this.tableData.map((todo) => {
+        const isEdit = todo.isEdit === true
+        const $editDisplay = isEdit ? 'none' : 'inline-block'
+        const $saveDisplay = isEdit ? 'inline-block' : 'none'
+        const $value = todo.value
         return `<li>
-        <input type="text" value="${todo}" disabled />
-        <button data-todo="${todo}" data-category="edit">修改</button>
-        <button style="display:none" data-todo="${todo}" data-category="save">保存</button>
-        <button data-todo="${todo}" data-category="del">删除</button>
+        <input type="text" value="${$value}" ${isEdit ? '' : 'disabled'} />
+        <button data-value="${$value}" data-category="edit" style="display:${$editDisplay}" >修改</button>
+        <button style="display:${$saveDisplay}" data-value="${$value}" data-category="save">保存</button>
+        <button data-value="${$value}" data-category="del">删除</button>
         </li>`
       })
       const ul = this.shadowRoot.querySelector('ul')
@@ -33,40 +37,38 @@
       const { target } = e
       const { tagName } = target
       if (tagName.toLocaleLowerCase() !== 'button') return
-      const { todo, category } = target.dataset
-      const $index = this.tableData.findIndex((item) => item === todo)
-      // 删除时找到index传递到父组件处理
-      if (category === 'del') {
+      const { value, category } = target.dataset
+      console.log(value)
+      const $index = this.tableData.findIndex((item) => item.value === value)
+      console.log($index)
+      console.log(JSON.stringify({ detail: { index: $index } }))
+      if (category === 'save') {
+        // 点击保存的时候判断更改后的值不与其他相同,然后交给父组件处理
+        const $liEl = target.parentNode
+        const $value = $liEl.children[0].value.trim()
+        if (!$value) {
+          return alert('不可修改为空,如不需要可删除!')
+        }
+        this.dispatchEvent(new CustomEvent('save-todo', {
+          detail: {
+            index: $index,
+            value: $value
+          }
+        }))
+        // 删除时找到index传递到父组件处理
+      } else if (category === 'del') {
         this.dispatchEvent(new CustomEvent('del-todo', {
           detail: {
             index: $index
           }
         }))
       } else {
-        const $liEl = target.parentNode
-        const $children = $liEl.children
-        if (category === 'edit') {
-          // 点击编辑时取消input的disabled属性
-          $children[0].disabled = false
-          $children[1].style.display = 'none'
-          $children[2].style.display = 'inline-block'
-        } else {
-          // 点击保存的时候判断更改后的值不与其他相同,然后交给父组件处理
-          const $value = $children[0].value.trim()
-          const isRepetition = this.tableData.find((item, index) => index !== $index && $value === item)
-          if (!$value) {
-            return alert('不可修改为空,如不需要可删除!')
+        // 点击编辑时取消input的disabled属性
+        this.dispatchEvent(new CustomEvent('edit-todo', {
+          detail: {
+            index: $index
           }
-          if (isRepetition) {
-            return alert('与已存在的代办重复,请修改!')
-          }
-          this.dispatchEvent(new CustomEvent('edit-todo', {
-            detail: {
-              index: $index,
-              value: $value
-            }
-          }))
-        }
+        }))
       }
     }
   }
